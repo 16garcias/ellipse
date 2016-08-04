fa9197f60b23fb09239ff8668476672311ee30d3
lkj <lkj>
2016-08-04 09:50:52 -0400

    ellipse

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function square(x, y, side) {
+rect(x,y,side,side);
+}
+
+function setup() {
+  createCanvas(100,150);
+rect(10, 10, 10, 15);
+fill(10,100,200);
+rect(40, 40, 40, 50);
+fill(51, 153, 255);
+ square (20, 19, 19);
+ fill (204, 102,255);
+ellipse(55,25,15)
+}
+
+function draw() {
+  
+}
